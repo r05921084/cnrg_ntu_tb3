513c785ed6854e131c248790d18f1f373743562f
Edward Chen <r05942027@ntu.edu.tw>
2018-04-04 15:42:44 +0800

    improve ipem yield loop

--- ipem_module/scripts/IPEM_pipe.py
+++ ipem_module/scripts/IPEM_pipe.py
@@ -1,66 +1,41 @@
-import os, sys
+import os
 import threading
 import time
 import subprocess
 
-# FIFO_PATH = '/tmp/ipem'
-# FIFO_PATH = '.'
-# FIFO_PCM = 'ipem_pcm'
-# FIFO_ANI = 'ipem_ani'
-# FIFO_PCM_WITH_PATH = FIFO_PATH + '/' + FIFO_PCM
-# FIFO_ANI_WITH_PATH = FIFO_PATH + '/' + FIFO_ANI
 
 class IPEM_pipe:
-    def __init__(self, channel_number=40, first_freq=2.0, freq_distance=0.5, sample_frequency=None,
+    def __init__(self, channel_number=40, first_freq=2.0, freq_distance=0.5, sample_frequency=22050,
                  new_ani_callback=None, raw_ani_string=True, fifo_path='.'):
         self.channel_number = channel_number
         self.first_freq = first_freq
         self.freq_distance = freq_distance
         self.sample_frequency = sample_frequency
         self.new_ani_callback = new_ani_callback
-        self.FIFO_PATH = fifo_path
-        self.FIFO_PCM = 'ipem_pcm'
-        self.FIFO_ANI = 'ipem_ani'
-        self.FIFO_PCM_WITH_PATH = self.FIFO_PATH + '/' + self.FIFO_PCM
-        self.FIFO_ANI_WITH_PATH = self.FIFO_PATH + '/' + self.FIFO_ANI
+        self.fifo_path = fifo_path
+        self.pcm_name = 'ipem_pcm'
+        self.ani_name = 'ipem_ani'
+        self.pcm_fullpath = self.fifo_path + '/' + self.pcm_name
+        self.ani_fullpath = self.fifo_path + '/' + self.ani_name
 
         try:
-            os.makedirs(self.FIFO_PATH)
+            os.makedirs(self.fifo_path)
         except OSError as oe:
-            print oe
+            pass
 
         try:
-            os.mkfifo(self.FIFO_PCM_WITH_PATH)
+            os.mkfifo(self.pcm_fullpath)
         except OSError as oe:
-            print oe
+            pass
 
         try:
-            os.mkfifo(self.FIFO_ANI_WITH_PATH)
+            os.mkfifo(self.ani_fullpath)
         except OSError as oe:
-            print oe
-
-        def pcm_feeder_generator():
-            with open(self.FIFO_PCM_WITH_PATH, 'wb') as fifo_pcm:
-                print("%s opened" % self.FIFO_PCM)
-                while True:
-                    samples = (yield 0)
-                    # print 'feed %d bytes' % len(samples)
-                    try:
-                        fifo_pcm.write(samples)
-                        fifo_pcm.flush()
-                    except IOError:
-                        print 'closing pcm_feeder due to fifo IOError'
-                        return
-
-                    if (yield 1):
-                        print 'closing pcm_feeder due to close()'
-                        return
-
-        self.pcm_feeder = pcm_feeder_generator()
+            pass
 
         def ani_receiver():
-            with open(self.FIFO_ANI_WITH_PATH, 'rb') as fifo_ani:
-                print("%s opened" % self.FIFO_ANI)
+            with open(self.ani_fullpath, 'rb') as fifo_ani:
+                print("%s opened" % self.ani_name)
                 while True:
                     ani_str = fifo_ani.readline()
                     # print('READ(%d): %s' % (len(ani_str), ani_str))
@@ -73,25 +48,23 @@ class IPEM_pipe:
                     else:
                         break
 
-            print('"%s" closed' % self.FIFO_ANI_WITH_PATH)
+            print('"%s" closed' % self.ani_fullpath)
 
         self.ani_receiver_thread = threading.Thread(target=ani_receiver)
         # self.ani_receiver_thread.setDaemon(True)
         self.ani_receiver_thread.start()
 
         def ipem_process():          
-            abspathname = os.path.abspath(os.path.dirname(sys.argv[0]))
-            print 'full path =', abspathname
-            print os.getcwd()
+            abspathname = os.path.abspath(os.path.dirname(__file__)) + '/'
             try:                
                 t1 = time.time()
-                log_str = subprocess.check_output([abspathname + '/' + 'IPEMAuditoryModelConsole',
-                                                   '-nc', str(40),
-                                                   '-if', self.FIFO_PCM,
-                                                   '-id', self.FIFO_PATH,
-                                                   '-of', self.FIFO_ANI,
-                                                   '-od', self.FIFO_PATH,
-                                                   '-fs', str(22050),
+                log_str = subprocess.check_output([abspathname + 'IPEMAuditoryModelConsole',
+                                                   '-nc', str(self.channel_number),
+                                                   '-if', self.pcm_name,
+                                                   '-id', self.fifo_path,
+                                                   '-of', self.ani_name,
+                                                   '-od', self.fifo_path,
+                                                   '-fs', str(self.sample_frequency),
                                                    '-ff', 'pcm'])
                 print log_str
                 print 'subprocess returned in %f sec' % (time.time() - t1)
@@ -102,20 +75,35 @@ class IPEM_pipe:
         # self.ipem_process_thread.setDaemon(True)
         self.ipem_process_thread.start()
 
+        def pcm_feeder_generator():
+            with open(self.pcm_fullpath, 'wb') as fifo_pcm:
+                print("%s opened" % self.pcm_name)
+                while True:
+                    samples = yield
+                    if samples is None:
+                        print 'closing pcm_feeder due to close()'
+                        return
+                    try:
+                        fifo_pcm.write(samples)
+                        fifo_pcm.flush()
+                    except IOError:
+                        print 'closing pcm_feeder due to fifo IOError'
+                        return
+
+        self.pcm_feeder = pcm_feeder_generator()
+        self.pcm_feeder.next()
+
     def feed_pcm_samples(self, samples):
         try:
-            self.pcm_feeder.next()
             self.pcm_feeder.send(samples)
-        except StopIteration as e:
-            pass
+        except StopIteration:
+            return False
+        else:
+            return True
 
     def close(self):
         try:
-            self.pcm_feeder.send(True)
-        except TypeError:
-            self.pcm_feeder.next()
-            self.pcm_feeder.send(True)
-            self.pcm_feeder.send(True)
+            self.pcm_feeder.send(None)
         except StopIteration:
             pass
         finally:
@@ -175,9 +163,9 @@ if __name__ == '__main__':
 #     log_str = subprocess.check_output(['./IPEMAuditoryModelConsole',
 #                                        '-nc', str(40),
 #                                        '-if', FIFO_PCM,
-#                                        '-id', FIFO_PATH,
+#                                        '-id', fifo_path,
 #                                        '-of', FIFO_ANI,
-#                                        '-od', FIFO_PATH,
+#                                        '-od', fifo_path,
 #                                        '-fs', str(44100),
 #                                        '-ff', 'pcm'])
 #     print log_str
--- ipem_module/scripts/IPEM_pipe_reactor.py
+++ ipem_module/scripts/IPEM_pipe_reactor.py
@@ -7,14 +7,18 @@ from std_msgs.msg import String
 
 
 RATE = 22050
+FIFO_PATH = '/tmp/IPEM'
+NODE_NAME = 'IPEM_pipe_reactor'
+SUB_TOPIC_NAME = 'audio_stream_raw'
+PUB_TOPIC_NAME = 'audio_stream_ani'
 
 
 def reactor():
-    rospy.init_node('IPEM_pipe_reactor', anonymous=False)
+    rospy.init_node(NODE_NAME, anonymous=False)
 
-    pub_L = rospy.Publisher('audio_stream_ani_L', String, queue_size=1)
-    pub_R = rospy.Publisher('audio_stream_ani_R', String, queue_size=1)
-    rospy.loginfo('"%s" starts subscribing to "%s".' % (NODE_NAME, TOPIC_NAME))
+    pub_L = rospy.Publisher(PUB_TOPIC_NAME + '/L', String, queue_size=1)
+    pub_R = rospy.Publisher(PUB_TOPIC_NAME + '/R', String, queue_size=1)
+    rospy.loginfo('"%s" starts subscribing to "%s".' % (NODE_NAME, SUB_TOPIC_NAME))
 
     def ipem_L_cb(data):
         pub_L.publish(data)
@@ -22,17 +26,17 @@ def reactor():
     def ipem_R_cb(data):
         pub_R.publish(data)
 
-    ipem_L = IPEM_pipe(new_ani_callback=ipem_L_cb, sample_frequency=RATE, fifo_path='/tmp/IPEM/L')
-    ipem_R = IPEM_pipe(new_ani_callback=ipem_R_cb, sample_frequency=RATE, fifo_path='/tmp/IPEM/R')
+    ipem_L = IPEM_pipe(new_ani_callback=ipem_L_cb, sample_frequency=RATE, fifo_path=FIFO_PATH + '/L')
+    ipem_R = IPEM_pipe(new_ani_callback=ipem_R_cb, sample_frequency=RATE, fifo_path=FIFO_PATH + '/R')
 
-    def callback(data):
+    def feed_ipem(data):
         global frames
         np_data = np.fromstring(data.data, dtype=np.int16).reshape([-1, 2])
-        # rospy.loginfo(np_data.shape)
         ipem_L.feed_pcm_samples(np_data[:, 0].tostring())
         ipem_R.feed_pcm_samples(np_data[:, 1].tostring())
+        rospy.loginfo(np_data.shape)
 
-    rospy.Subscriber('audio_stream_raw', String, callback)
+    rospy.Subscriber(SUB_TOPIC_NAME, String, feed_ipem)
 
     try:
         rospy.spin()
@@ -40,8 +44,9 @@ def reactor():
         ipem_L.close()
         ipem_R.close()
 
+
 if __name__ == '__main__':
     try:
         reactor()
     except rospy.ROSInterruptException as e:
-        rospy.loginfo(e)
+        rospy.logerror(e)
