e11044a65a4bfbff15f922717db083654ff3216d
Edward Chen <r05942027@ntu.edu.tw>
2018-04-10 01:34:27 +0800

    add '/audio_stream_raw' topic back, tweak 'IPEM_pipe_reactor'
    add '/audio_stream_raw' for temporary backward compatibility, will soon be removed in future.

--- binaural_microphone/scripts/webcam_stereo.py
+++ binaural_microphone/scripts/webcam_stereo.py
@@ -2,9 +2,11 @@
 import pyaudio
 # import wave
 import numpy as np
+import time
 
 import rospy
 from std_msgs.msg import Float32
+from std_msgs.msg import String
 from binaural_microphone.msg import BinauralAudio
 
 
@@ -30,24 +32,26 @@ if __name__ == '__main__':
         rospy.init_node(NODE_NAME, anonymous=False)
 
         raw_pub = rospy.Publisher(TOPIC_NAME, BinauralAudio, queue_size=1)
+        raw_str_pub = rospy.Publisher('/audio_stream_raw', String, queue_size=1)  # for backward compability.
         rms_L_pub = rospy.Publisher(RMS_TOPIC_NAME + '/L', Float32, queue_size=1)
         rms_R_pub = rospy.Publisher(RMS_TOPIC_NAME + '/R', Float32, queue_size=1)
 
         rospy.loginfo('"%s" starts publishing to "%s".' % (NODE_NAME, TOPIC_NAME))
         
         while not rospy.is_shutdown():
-            data = stream.read(CHUNK_SIZE)
-            np_data = np.fromstring(data, dtype=np.int16).reshape([-1, 2])        
-            rospy.loginfo(np_data.shape)
             ba = BinauralAudio()
-            ba.header.stamp = rospy.Time.now()
             ba.header.frame_id = NODE_NAME
             ba.type = 'PCM Int16'
             ba.sample_rate = SAMPLE_RATE
             ba.chunk_size = CHUNK_SIZE
+
+            raw_str = stream.read(CHUNK_SIZE)
+            np_data = np.frombuffer(raw_str, dtype=np.int16).reshape([-1, 2])            
+            ba.header.stamp = rospy.Time.now()            
             ba.left_channel = np_data[:, 0]
             ba.right_channel = np_data[:, 1]                        
             raw_pub.publish(ba)
+            raw_str_pub.publish(raw_str)
 
             rms_L_pub.publish(np.sqrt(np.mean(np.square(np_data[:, 0].astype(np.float)))))
             rms_R_pub.publish(np.sqrt(np.mean(np.square(np_data[:, 1].astype(np.float)))))
