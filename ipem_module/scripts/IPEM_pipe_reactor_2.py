19dbf8ddf4d447321cd837a36bfd4885a03bd97d
Edward Chen <r05942027@ntu.edu.tw>
2018-04-16 10:51:51 +0800

    improve ipem reactor & visualizer efficiency
    Former-commit-id: 04b5c86c7cfd0899af3e6980306eb60d4492b12b [formerly 2e1d9845eb56a42e4c9a6cba368cbbe0c6ec76e8]
    Former-commit-id: c97a5edaa89671205b04483933deaf68eb5a84d5
    Former-commit-id: 0c946817cf0ae1451312156e20bb09c5dcc14038

--- ipem_module/scripts/IPEM_pipe_reactor_2.py
+++ ipem_module/scripts/IPEM_pipe_reactor_2.py
@@ -24,23 +24,36 @@ def reactor():
     rospy.init_node(NODE_NAME, anonymous=False)
 
     ani_pub = rospy.Publisher(PUB_TOPIC_NAME, AuditoryNerveImage, queue_size=1)
-    ipem_L_results = deque()
+        
+    global ipem_L_count
+    ipem_L_count = 0
+    ipem_L_results = np.zeros([CHUNK_SIZE * N_SUBCHANNELS])
     ipem_L_ready = Event()
 
     def ipem_L_cb(data):
-        np_data = np.fromstring(data, dtype=np.float, count=N_SUBCHANNELS, sep=' ')
-        ipem_L_results.append(np_data)
-        if len(ipem_L_results) > CHUNK_SIZE:
-            ipem_L_ready.set()
-
-    ipem_R_results = deque()
+        if not ipem_L_ready.is_set():
+            global ipem_L_count
+            offset = ipem_L_count * N_SUBCHANNELS
+            ipem_L_results[offset:offset + N_SUBCHANNELS] = np.fromstring(data, dtype=np.float, count=N_SUBCHANNELS, sep=' ')
+            ipem_L_count += 1
+            if ipem_L_count == CHUNK_SIZE:
+                ipem_L_ready.set()
+                ipem_L_count = 0
+    
+    global ipem_R_count
+    ipem_R_count = 0
+    ipem_R_results = np.zeros([CHUNK_SIZE * N_SUBCHANNELS])
     ipem_R_ready = Event()
 
     def ipem_R_cb(data):
-        np_data = np.fromstring(data, dtype=np.float, count=N_SUBCHANNELS, sep=' ')
-        ipem_R_results.append(np_data)
-        if len(ipem_R_results) > CHUNK_SIZE:
-            ipem_R_ready.set()
+        if not ipem_R_ready.is_set():
+            global ipem_R_count
+            offset = ipem_R_count * N_SUBCHANNELS
+            ipem_R_results[offset:offset + N_SUBCHANNELS] = np.fromstring(data, dtype=np.float, count=N_SUBCHANNELS, sep=' ')
+            ipem_R_count += 1
+            if ipem_R_count == CHUNK_SIZE:
+                ipem_R_ready.set()
+                ipem_R_count = 0
 
     ipem_L = IPEM_pipe(new_ani_callback=ipem_L_cb, sample_frequency=SAMPLE_RATE, fifo_path=FIFO_PATH + '/L')
     ipem_R = IPEM_pipe(new_ani_callback=ipem_R_cb, sample_frequency=SAMPLE_RATE, fifo_path=FIFO_PATH + '/R')
@@ -58,13 +71,7 @@ def reactor():
     try:
         # rospy.spin()
         while not rospy.is_shutdown():
-            while ipem_L_ready.wait(1) and ipem_R_ready.wait(1):
-                ipem_L_list = [ipem_L_results.popleft() for _ in range(CHUNK_SIZE)]
-                ipem_R_list = [ipem_R_results.popleft() for _ in range(CHUNK_SIZE)]
-                ipem_L_np = np.hstack(ipem_L_list)
-                ipem_R_np = np.hstack(ipem_R_list)
-                ipem_L_ready.clear()
-                ipem_R_ready.clear()
+            while ipem_L_ready.wait(1) and ipem_R_ready.wait(1):                  
                 ani = AuditoryNerveImage(
                     header=Header(
                         stamp=rospy.Time.now()
@@ -72,9 +79,11 @@ def reactor():
                     sample_rate=SAMPLE_RATE,
                     chunk_size=CHUNK_SIZE,
                     n_subchannels=N_SUBCHANNELS,
-                    left_channel=ipem_L_np,
-                    right_channel=ipem_R_np
+                    left_channel=ipem_L_results,
+                    right_channel=ipem_R_results.flatten()
                 )
+                ipem_L_ready.clear()
+                ipem_R_ready.clear()
                 ani_pub.publish(ani)
 
             rospy.loginfo('main loop time out!')
