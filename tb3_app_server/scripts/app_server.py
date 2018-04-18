3bbc8becacd523ddd7ea7d581ed5e4c3661e2c02
TB3 <r05942027@ntu.edu.tw>
2018-04-18 19:00:05 +0800

    update app_server and core launch
    Auto stash before merge of "dev" and "origin/dev"
    
    Former-commit-id: 59bbfe948a17ada1487b75685b5d63c9d0eba807 [formerly 8711c65b195d1eaaf3d1efcdb1f0c0cada7a4bcd]
    Former-commit-id: f3111df74a62ca46c4aae6fc73d29f7f687cf9e5
    Former-commit-id: 38e2a146ef4425b30964855c510e9234f3baba8b

--- tb3_app_server/scripts/app_server.py
+++ tb3_app_server/scripts/app_server.py
@@ -8,65 +8,86 @@ from collections import deque
 import roslaunch
 import time
 
-launches = {}
-to_be_launch = deque()
-new_event = threading.Event()
+
+supported_cmd = {}
+supported_cmd['card_game'] = ('card_game', '/launch/card_game.launch')
+supported_cmd['text_analysis'] = ('keyboard_io', '/launch/text_analysis.launch')
+supported_cmd['ipem_visual_split'] = ('ipem_module', '/launch/visual_split.launch')
+
+supported_op = ['run', 'end']
 
 
-def launch_app(pkg, launch_file):
+def launch_app(key, pkg=None, launch_file=None):
+    if pkg is None or launch_file is None:
+        try:
+            (pkg, launch_file) = supported_cmd[key]
+        except KeyError:
+            rospy.logwarn('not supported app: %s' % key)
+            return
     path = rospack.get_path(pkg)
     launch = roslaunch.parent.ROSLaunchParent(uuid, [path + launch_file])
-    launches[pkg] = launch
-    launch.start()
-    rospy.loginfo('launch_app' + pkg + launch_file)
+    try:
+        launch.start()
+        launched_app[key] = launch
+        rospy.loginfo('launch_app: [%s] %s, %s' % (key, pkg, path + launch_file))
+    except roslaunch.RLException as e:
+        rospy.logwarn(e)
+
 
 def close_app(key):
     try:
-        launches[key].shutdown()
-        del launches[key]
-        rospy.loginfo('close_app %s' % key)
+        launched_app[key].shutdown()
+        del launched_app[key]
+        rospy.loginfo('close_app [%s]' % key)
     except KeyError:
         pass
 
 
-def call_back(data):
-    to_be_launch.append(data.data)
-    new_event.set()
+def new_cmd_callback(data):
+    
+    cmd = [key for key in supported_cmd if key in data.data]
+    op = [op for op in supported_op if op in data.data]
+
+    if len(cmd) and len(op):        
+        cmd_queue.append((cmd[0], op[0]))
+        new_event.set()
 
 
 def close_all_launch():
-    for key in launches:
+    for key in launched_app:
         close_app(key)
 
 
 def run_app_server():
-    rospy.init_node('roslaunch_api_test', anonymous=True)
+    global uuid, rospack, launched_app, cmd_queue, new_event
 
-    global uuid, rospack
+    launched_app = {}
+    cmd_queue = deque()
+    new_event = threading.Event()
+
+    rospy.init_node('tb3_app_server', anonymous=False)
+    rospack = rospkg.RosPack()
+    
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     roslaunch.configure_logging(uuid)
 
-    rospy.Subscriber('chatter', String, call_back)
+    rospy.Subscriber('chatter', String, new_cmd_callback)
+    
     rospy.on_shutdown(close_all_launch)
-    rospack = rospkg.RosPack()
 
-    while (not rospy.is_shutdown()) and new_event.wait():
-        if len(to_be_launch):
-            cmd = to_be_launch.popleft()
+    while not rospy.is_shutdown():
+        while new_event.wait(0.5):
+            cmd, op = cmd_queue.popleft()
+            new_event.clear()
 
-            if cmd == 'run card_game':
-                launch_app('card_game', '/launch/card_game.launch')
-                
-            elif cmd == 'end card_game':
-                close_app('card_game')
+            print cmd, op
 
-            elif cmd == 'run text_analysis':
-                launch_app('keyboard_io', '/launch/text_analysis.launch')
+            if op == 'run':
+                launch_app(cmd)
+            elif op == 'end':
+                close_app(cmd)
 
-            elif cmd == 'end text_analysis':
-                close_app('keyboard_io')
-
-            print launches
+            print launched_app
 
 
 if __name__ == '__main__':
