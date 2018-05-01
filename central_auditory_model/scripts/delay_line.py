23598d90131a673769ac623d6cbea54f2ad4250b
Edward Chen <r05942027@ntu.edu.tw>
2018-05-01 12:30:03 +0800

    update delay_line module
    Former-commit-id: eedddc4b49148b96c4cafe83ae402692cfd9eaa4 [formerly e885d848de0e76ac38cf7811d92786728c5b2315]
    Former-commit-id: 97ec83664b511dabc511833c614c82a95e6e004c
    Former-commit-id: be4a9c320ae1cb5c7b2e3e88a50f4edcd37df952

--- central_auditory_model/scripts/delay_line.py
+++ central_auditory_model/scripts/delay_line.py
@@ -1,48 +1,43 @@
 import numpy as np
+import warnings
 
 class DelayLine(object):
     def __init__(self, data_dim, sample_rate, max_delay=1.0, initial_value=0.):
         self.data_dim = data_dim
         self.sample_rate = float(sample_rate)
         self.max_delay = max_delay
-        self.length = int(max_delay * sample_rate)
-        self.buffer = initial_value * np.ones((self.length,) + tuple(data_dim))
-        self.current_step = 0
-        self.head = 0
-
-        print self.buffer.shape
+        self.buf_len = int(max_delay * sample_rate)
+        self.buffer = initial_value * np.ones((self.buf_len,) + tuple(data_dim))
+        self.buf_head = 0
+        self.n_steps = 0
 
     def update(self, data):
-        # print data.shape
-        # if data.shape[0] > self.length:
-        #     print 'buffer overflow: updating %d steps while buffer is only %d steps.' % (data.shape[0], self.length)
-        #     return
+        if data.shape[0] > self.buf_len:
+            raise ValueError('buffer overflow: updating %d steps while buffer is only %d steps.' % (data.shape[0], self.buf_len))
 
-        stop = self.head + data.shape[0]
-        wrapped_index = np.arange(self.head, stop)
-        wrapped_index[wrapped_index >= self.length] -= self.length
-        self.head = (stop - self.length) if stop >= self.length else stop
+        stop = self.buf_head + data.shape[0]
+        wrapped_index = np.arange(self.buf_head, stop)
+        wrapped_index[wrapped_index >= self.buf_len] -= self.buf_len
+        self.buf_head = (stop - self.buf_len) if stop >= self.buf_len else stop
 
         self.buffer[wrapped_index] = data
 
-        self.current_step += data.shape[0]        
-        # print 'head', self.head, self.current_step / self.sample_rate
+        self.n_steps += data.shape[0]
         
 
     def delayed_view(self, delay):
 
         def view(t):
-            steps_behind = self.current_step - int((t - delay) * self.sample_rate)
-            if steps_behind > self.length:
-                # print steps_behind, 'already forgot!'
-                return self.buffer[0]
+            steps_behind = self.n_steps - int((t - delay) * self.sample_rate)
+            if steps_behind > self.buf_len:
+                warnings.warn('Buffer already forgot!', Warning)
+                return self.buffer[self.buf_head]
             elif steps_behind <= 0:
-                # print steps_behind, 'not there yet!'
-                return self.buffer[0]
+                warnings.warn('Buffer is not there yet!', Warning)
+                return self.buffer[self.buf_head - 1]
             else:
-                index = self.head - steps_behind
-                index = index + self.length if index < 0 else index
-                # print 'view', index, steps_behind
+                index = self.buf_head - steps_behind
+                index = index + self.buf_len if index < 0 else index
                 return self.buffer[index]
 
         return view
