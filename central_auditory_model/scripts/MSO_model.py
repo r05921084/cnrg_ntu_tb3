d351e25f23475277ed5aecc46face464140fe82f
Edward Chen <r05942027@ntu.edu.tw>
2018-05-01 17:57:06 +0800

    update MSO_model working code
    Former-commit-id: e624c14e9c84a10378680a2c881a588cb24394af [formerly 8fc714b3e1c8b2131e7fd89fe825bc21636a97bc]
    Former-commit-id: b9755ed3b136af3a743ec33e8e4fe235e267d3c3
    Former-commit-id: 394a601bb6b6499d9b52e29343dd37fdd0ebdf04

--- central_auditory_model/scripts/MSO_model.py
+++ central_auditory_model/scripts/MSO_model.py
@@ -4,6 +4,7 @@ import time
 import threading
 import nengo
 from delay_line import DelayLine
+from signal_collector import SignalCollector
 
 import rospy
 from std_msgs.msg import String
@@ -13,12 +14,27 @@ from ipem_module.msg import AuditoryNerveImage
 
 
 NODE_NAME = 'nengo_mso_model'
-PUB_TOPIC_NAME = 'mso_stream'
+PUB_TOPIC_NAME = '/central_auditory_model/mso_stream'
 SUB_TOPIC_NAME = '/ipem_module/apm_stream'
 SAMPLE_RATE = 11025
 CHUNK_SIZE = 1024
 N_SUBCHANNELS = 40
-MAX_DELAY = 10.
+MAX_DELAY = 5.
+MAX_STEPS = int(MAX_DELAY * SAMPLE_RATE)
+
+DELAY_VALUES = [0., 9.e-5, 26e-5, 41e-5, 56e-5, 73e-5, 82e-5]
+N_DELAY_VAL = len(DELAY_VALUES)
+MAXPOOLING = 100
+# DELAY_VALUES = [0., 41e-5, 82e-5]
+
+
+synapse1 = None
+synapse2 = 0
+synapse3 = 0
+synapse4 = None
+
+radius_1 = 1
+radius_2 = 2        
 
 
 def run_MSO_model():
@@ -28,52 +44,49 @@ def run_MSO_model():
 
     mso_pub = rospy.Publisher(PUB_TOPIC_NAME, AuditoryNerveImage, queue_size=1)
 
-    reshape_index = np.arange(1024)[:,np.newaxis] * 40 + np.arange(40)
-    ani_L = np.zeros([N_SUBCHANNELS * CHUNK_SIZE])
-    ani_R = np.zeros([N_SUBCHANNELS * CHUNK_SIZE])
+    ani_L = np.zeros([CHUNK_SIZE, N_SUBCHANNELS])
+    ani_R = np.zeros([CHUNK_SIZE, N_SUBCHANNELS])
+    ani_L_1d = ani_L.ravel() # create an 1-D view into ani_L, must use ravel().
+    ani_R_1d = ani_R.ravel() # create an 1-D view into ani_L, must use ravel().
+
     event = threading.Event()
 
-    def ani_cb(data):
-        
+    def ani_cb(data):        
         if data.chunk_size != CHUNK_SIZE or data.n_subchannels != N_SUBCHANNELS or data.sample_rate != SAMPLE_RATE:            
-            rospy.logwarn('NOT IMPLEMENT YET: variable CHUNK_SIZE, N_SUBCHANNELS and SAMPLE_RATE not supported!')
+            rospy.logwarn('NOT IMPLEMENT YET: dynamic CHUNK_SIZE, N_SUBCHANNELS and SAMPLE_RATE not supported!')
             # CHUNK_SIZE = data.chunk_size
             # N_SUBCHANNELS = data.n_subchannels
             # SAMPLE_RATE = data.sample_rate
             # setup_objects()
             return
-        # t1 = time.time()
+        t1 = time.time()
         try:
-            # ani_L[:] = np.asarray(data.left_channel).reshape([data.chunk_size, data.n_subchannels])
-            # ani_R[:] = np.asarray(data.right_channel).reshape([data.chunk_size, data.n_subchannels])
-            ani_L[:] = data.left_channel
-            ani_R[:] = data.right_channel
+            ani_L_1d[:] = data.left_channel
+            ani_R_1d[:] = data.right_channel
         except ValueError:
             rospy.logwarn('shape mismatch: %d -> %d %d' % (len(data.left_channel), data.chunk_size, data.n_subchannels))
             return
-        
-        dl_L.update(ani_L[reshape_index])
-        dl_R.update(ani_R[reshape_index])
-        event.set()
+        else:
+            dl_L.update(ani_L)
+            dl_R.update(ani_R)
+            event.set()
         # print time.time() - t1
 
+    mso_msg = AuditoryNerveImage(sample_rate=SAMPLE_RATE / MAXPOOLING, chunk_size=N_DELAY_VAL, n_subchannels=N_SUBCHANNELS)
+
+    def mso_output_cb(t, x):
+        mso_msg.header = Header(stamp=rospy.Time.now())
+        mso_msg.left_channel = x.ravel()
+        mso_pub.publish(mso_msg)
+
+    sc = SignalCollector((N_DELAY_VAL, N_SUBCHANNELS), mso_output_cb, initial_value=0.05, maxpooling=100)
+
     def build_model():
-        synapse1 = None
-        synapse2 = 0
-        synapse3 = 0
-        synapse4 = None
-
-        radius_1 = 1
-        radius_2 = 2
-
         lifrate_model = nengo.LIFRate(tau_rc=0.002, tau_ref=0.0002)
         max_r = nengo.dists.Uniform(1000, 2000)
 
-        delay_values = [0., 9.e-5, 26e-5, 41e-5, 56e-5, 73e-5, 82e-5]
-        # delay_values = [0., 9.e-5, 26e-5]
-
-        def MSO_ensemble_array(radius, label='ens_arr'):
-            return nengo.networks.EnsembleArray(n_neurons=32, n_ensembles=N_SUBCHANNELS, ens_dimensions=1, label=label,
+        def MSO_ensemble_array(dim, radius, label='ens_arr'):
+            return nengo.networks.EnsembleArray(n_neurons=32, n_ensembles=dim, ens_dimensions=1, label=label,
                                                 radius=radius, neuron_type=lifrate_model, max_rates=max_r, seed=None)
 
         model = nengo.Network(label="MSO_Jeffress_Model_Multi_Channel")
@@ -83,41 +96,37 @@ def run_MSO_model():
             ens_arr_L = []
             ens_arr_R = []
             ens_arr_add = []
+            output_node = []
 
-            input_node_L_probe = []
-            input_node_R_probe = []
-            ens_arr_add_probe = []
+            dim = N_SUBCHANNELS
 
-            for i in range(len(delay_values)):
-                input_node_L.append(nengo.Node(dl_L.delayed_view(delay_values[i]), label='input_node_L[%d]' % i))
-                input_node_R.append(nengo.Node(dl_R.delayed_view(delay_values[i]), label='input_node_R[%d]' % i))
-                ens_arr_L.append(MSO_ensemble_array(radius_1, label='ens_arr_L[%d]' % i))
-                ens_arr_R.append(MSO_ensemble_array(radius_1, label='ens_arr_R[%d]' % i))
-                ens_arr_add.append(MSO_ensemble_array(radius_2, label='ens_arr_add[%d]' % i))
+            for i in range(N_DELAY_VAL):                
+                input_node_L.append(nengo.Node(dl_L.delayed_view(DELAY_VALUES[i]), 0, dim, 'input_node_L[%d]' % i))
+                input_node_R.append(nengo.Node(dl_R.delayed_view(DELAY_VALUES[i]), 0, dim, 'input_node_R[%d]' % i))
+                ens_arr_L.append(MSO_ensemble_array(dim, radius_1, 'ens_arr_L[%d]' % i))
+                ens_arr_R.append(MSO_ensemble_array(dim, radius_1, 'ens_arr_R[%d]' % i))
+                ens_arr_add.append(MSO_ensemble_array(dim, radius_2, 'ens_arr_add[%d]' % i))
+                output_node.append(nengo.Node(sc.updater_factory(i), N_SUBCHANNELS, 0, 'output_node[%d]' % i))
 
-            for i in range(len(delay_values)):
+            for i in range(N_DELAY_VAL):
                 nengo.Connection(input_node_L[i],   ens_arr_L[i].input, synapse=synapse2)
                 nengo.Connection(input_node_R[i],   ens_arr_R[i].input, synapse=synapse2)
                 nengo.Connection(ens_arr_L[i].output, ens_arr_add[i].input, synapse=synapse3)
                 nengo.Connection(ens_arr_R[-1-i].output, ens_arr_add[i].input, synapse=synapse3)
+                nengo.Connection(ens_arr_add[i].output, output_node[i], synapse=synapse4)
 
-
-            for i in range(len(delay_values)):
-                input_node_L_probe.append(nengo.Probe(input_node_L[i]))
-                input_node_R_probe.append(nengo.Probe(input_node_R[i]))
-                ens_arr_add_probe.append(nengo.Probe(ens_arr_add[i].output, synapse=synapse4))
 
         dt = 1. / SAMPLE_RATE
 
         # sim = nengo.Simulator(model, dt=dt)
 
         import nengo_dl
-        sim = nengo_dl.Simulator(model, dt=dt, unroll_simulation=128)
+        sim = nengo_dl.Simulator(model, dt=dt, unroll_simulation=1)
 
         # import os; os.environ['PYOPENCL_CTX'] = '0'
         # import pyopencl, nengo_ocl
         # ctx = pyopencl.create_some_context()
-        # sim = nengo_ocl.Simulator(model, context=ctx, dt=dt, n_prealloc_probes=1024)
+        # sim = nengo_ocl.Simulator(model, context=ctx, dt=dt)
 
         return sim
 
@@ -127,18 +136,22 @@ def run_MSO_model():
     rospy.Subscriber(SUB_TOPIC_NAME, AuditoryNerveImage, ani_cb)
     rospy.loginfo('"%s" starts subscribing to "%s".' % (NODE_NAME, SUB_TOPIC_NAME))
 
-    while not rospy.is_shutdown():
-        while event.wait(0.3):
+    while not rospy.is_shutdown() and event.wait(1.0):
+
+        yet_to_run = min([dl_L.n_steps, dl_R.n_steps]) - sim.n_steps - CHUNK_SIZE
+
+        if yet_to_run == 0:                
             event.clear()
-            steps_to_run = CHUNK_SIZE
-            t2 = time.time()
-            sim.run_steps(steps_to_run)
-            print 'ran %d steps in %5.3f sec' % (steps_to_run, time.time() - t2)
-            yet_to_run = dl_L.current_step - sim.n_steps
-            print 'yet_to_run: %d steps' % yet_to_run
-            if yet_to_run > MAX_DELAY * SAMPLE_RATE:
-                break
-        break
+        elif yet_to_run > MAX_STEPS:
+            print 'delay too much!'
+            break
+        elif yet_to_run < 0:
+            print 'yet_to_run < 0?'
+            break
+
+        t2 = time.time()
+        sim.run_steps(CHUNK_SIZE, progress_bar=False)
+        print 'ran %d steps in %5.3f sec, %d steps yet to run.' % (CHUNK_SIZE, time.time() - t2, yet_to_run)
 
 
 if __name__ == '__main__':
