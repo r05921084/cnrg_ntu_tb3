9b8dff867a708228d13a6d1d99eec4b7cf9fc763
TWLiu <r05921084@ntu.edu.tw>
2018-05-08 22:13:09 +0800

    add feedback analysis
    Former-commit-id: 34a9d0f29c49cc5feda8e6d7f88809df6630ee04 [formerly 2cea1f4919e8915947b0a2709f5083d660f10ad5]
    Former-commit-id: 5eeb444f01aa064c13664cb707a0956042a353d7
    Former-commit-id: b59b83f29ce9dfea78555ac3d327cf4a4e05693a

--- card_game/scripts/game_agent.py
+++ card_game/scripts/game_agent.py
@@ -27,6 +27,8 @@ msg = None
 
 card_num = 3
 
+key_map = {'apple':'蘋果', 'banana':'香蕉', 'cat':'貓', 'dog':'狗'}
+
 game_introduction = ["XXX您好,我們來玩遊戲吧！", "這裡有{}張牌,每張牌都有不同的圖案".format(card_num),
                     "等會兒我會把牌蓋起來", "看看你能記住幾張", "記得越多就可以拿到越多獎品喔！",
                     "知道怎麼玩了嗎？", "那我們就開始囉！"]
@@ -39,25 +41,26 @@ praise1 = ["很接近了", "可惜，差點就答對了"]
 
 test = ["請問這張牌是什麼呢？"]
 
+STATIC_PATH = os.path.join(os.path.dirname(__file__), 'static')
 
-filepath = os.path.dirname(os.path.abspath(__file__))
-cards_all = os.listdir(os.path.join(filepath, 'static', 'images'))
+cards_all = os.listdir(os.path.join(STATIC_PATH, 'images'))
 cards_all.remove('ques.png')
 cards_all.sort()
 
-f = open(os.path.join(filepath, 'card_property.json'), 'r')
+f = open(os.path.join(STATIC_PATH, 'card_property.json'), 'r')
 card_property = json.load(f)
 f.close()
 
 
 # define("ip", default="localhost")
-define("ip", default="172.16.0.103")
+define("ip", default="172.16.0.105")
 define("port", default=8888)
 
 
 class Chat(web.RequestHandler):
     def get(self):
-        self.render("index.html")
+        self.render("cardgame.html")
+
 
 class MyWebSocket(websocket.WebSocketHandler):
 
@@ -93,12 +96,47 @@ def get_feedback():
     # feedback = raw_input().decode('utf8')
     event.wait()
     event.clear()
-    if msg == 'q':
-        rospy.signal_shutdown()
-    else:
-        feedback = msg.decode('utf8')
+    feedback = msg.decode('utf8')
 
     return feedback
+
+
+def check_answer(s, key_):
+    for p in card_property[key_]:
+        if s.find(p)>=0:
+            return True
+    return False
+
+
+def analysis(statement, card_key):
+    # check the statement is corresponding to the card
+    correct_comment = ['您說得真好', '很棒', '很厲害喔', '沒錯，就是{}'.format(key_map[card_key]), '你喜歡{}嗎'.format(key_map[card_key])]
+    not_correct_comment = ['說得很好，可是好像不對喔', '差一點就講對了，還有沒有別的答案呢', '好像不太一樣耶，有沒有可能是{}呢'.format(key_map[card_key])]
+    dont_know_words = ['不知道', '不明白', '不曉得', '不清楚', '不懂' , '不會', '不能理解']
+    stop_words = ['不玩了', '不想玩了']
+    dont_know_comment = ['再想想看，你一定可以想起來的', '這是{}'.format(key_map[card_key])]
+    correct = False
+    dont_know = False
+    for p in card_property[card_key]:
+        if statement.find(p)>=0:
+            correct = True
+            break
+
+    if correct:
+        comment = random.choice(correct_comment)
+    else:
+        for a in dont_know_words:
+            comment = random.choice(not_correct_comment)
+            if statement.find(a.decode('utf8'))>=0:
+                comment = random.choice(dont_know_comment)
+                break
+
+    return correct, comment
+
+
+
+
+
 
 
 def agent(websocket):
@@ -125,6 +163,7 @@ def agent(websocket):
     send_command('init', card_num)
 
     response_time = np.zeros(card_num)
+
     # Loading image
     select_card = random.sample(cards_all, card_num)
     # print select_card
@@ -145,14 +184,19 @@ def agent(websocket):
     ## Describing the card
     # TODO: check if the answer is right
     for i in range(card_num):
+        right = False
+
         s_describe = random.choice(card_describe)
         send_command('ask', [str(i), s_describe])
         speak(s_describe)
-        feedback = get_feedback()
 
-        s_praise = random.choice(praise)
-        send_command('talk', s_praise)
-        speak(s_praise)
+        while not right:
+            feedback = get_feedback()
+            right, comment = analysis(feedback, select_card[i][:-4])
+            # s_praise = random.choice(praise)
+            print comment
+            send_command('talk', comment)
+            speak(comment)
         time.sleep(2)
 
 
@@ -165,24 +209,20 @@ def agent(websocket):
     ## Fold?
     send_command('fold')
 
-
+    accuracy = np.zeros(card_num) - 1
     ## Test?
-    def check_answer(s, key_):
-        for p in card_property[key_]:
-            if s.find(p)>=0:
-                return True
-        return False
-
-
-
     for i in range(card_num):
         s_test = random.choice(test)
         send_command('ask', [str(i), s_test])
         speak(s_test)
         t = time.time()
         ###
+
+        count = 0
+
         while True:
             feedback = get_feedback()
+            count += 1
 
             if check_answer(feedback, select_card[i][:-4]):
                 send_command('talk,答對了!')
@@ -200,13 +240,21 @@ def agent(websocket):
                 send_command('talk,再猜猜看')
                 speak("再猜猜看")
 
-
-        time.sleep(2)
+        accuracy[i] = (1. / count) * 100
+        time.sleep(1)
         response_time[i] = time.time() - t
 
     total_time = np.sum(response_time)
     send_command('talk,共花了%d秒'%total_time)
     # send_command('talk,共花了{}秒'.format(int(total_time)))
+    with open(os.path.join(STATIC_PATH, 'history.txt'), 'w') as f:
+        f.write(time.strftime("%Y-%m-%d %H:%M:%S\n"))
+        f.write('card, reaction time, accuracy\n')
+
+        for i in range(card_num):
+            f.write('{}, {:.1f} s, {:.2f}%\n'.format(select_card[i][:-4], response_time[i], accuracy[i]))
+
+
     rospy.signal_shutdown('Game Finished')
     
 
