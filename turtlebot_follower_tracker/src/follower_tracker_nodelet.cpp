ea28210dfb8d7d99bc2e2f373607171b6f2967a7
shtseng <q253upng@gmail.com>
2018-05-08 21:29:43 +0800

    Add follower tracker package and update meta package.xml
    Former-commit-id: a2cbecd8bb4bd08975b392b7d057adf947d87305 [formerly 0fc28eed3211b0557e521a7a7f8e9c0ca8c38495]
    Former-commit-id: f5b3865f505dbd3f22c2e05c34b9156eacfd9394
    Former-commit-id: 111d6a362f410f68a6411db154346a4673660635

---
+++ turtlebot_follower_tracker/src/follower_tracker_nodelet.cpp
@@ -0,0 +1,30 @@
+#include <turtlebot_follower_tracker/pcl_people_detection_ros.h>
+
+namespace turtlebot_guide
+{
+
+class FollowerTrackerNodelet : public nodelet::Nodelet
+{
+public:
+
+  FollowerTrackerNodelet(){}
+
+  ~FollowerTrackerNodelet(){}
+
+private:
+
+  virtual void onInit()
+  {
+    ROS_INFO("Load follower tracker nodelet");
+//    ftr_.reset(new FollowerTrackerROS(getNodeHandle(), getPrivateNodeHandle()));
+    pdr_.reset(new PCLPeopleDetectionROS(getNodeHandle(), getPrivateNodeHandle()));
+  }
+
+//  std::shared_ptr<turtlebot_guide::FollowerTrackerROS> ftr_;
+  std::shared_ptr<turtlebot_guide::PCLPeopleDetectionROS> pdr_;
+};
+
+}
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_DECLARE_CLASS(turtlebot_guide, FollowerTrackerNodelet, turtlebot_guide::FollowerTrackerNodelet, nodelet::Nodelet);
