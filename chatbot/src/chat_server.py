75d1184539a6adbe1bca8597714b1c188186fbee
TWLiu <r05921084@ntu.edu.tw>
2018-05-08 22:30:43 +0800

    [Chatbot] web interface v1.0
    Former-commit-id: 5d874fd5b0bd94730a05fb7c62fb922ec66d0474 [formerly 502ed163c2c8e9c69cff468fc2d8e0a4fa4a5bfe]
    Former-commit-id: ca6cdb63aee0b9e5f31462b4f6610961f4662ff8
    Former-commit-id: 1313ae2971bfb80209056b5356be22a27e0c9f98

---
+++ chatbot/src/chat_server.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env/ python
+# -*- coding: utf-8 -*-
+import os
+from tornado import web, ioloop, websocket
+from tornado.options import define, options
+
+define("ip", default='localhost')
+define("port", default=8888)
+
+# 此物件將記錄並管理所有連線
+class ChatManager(object):
+    users = []
+    @classmethod
+    def add_user(cls, websocket):
+        cls.users.append(websocket)
+
+    @classmethod
+    def remove_user(cls, websocket):
+        cls.users.remove(websocket)
+
+# 輸出聊天室 UI 畫面
+class Chat(web.RequestHandler):
+    def get(self):
+        self.render("chat.html")
+
+# 利用 WebSocketHandler 來與 Client 端互動
+class Socket(websocket.WebSocketHandler):
+    def open(self):
+        # 完成連線，將此 WebSocket 儲存
+        print ' [x] connected.'
+        ChatManager.add_user(self)
+
+    def on_close(self):
+        # 連線結束，將此 WebSocket 移除
+        print ' [x] disconnected.'
+        ChatManager.remove_user(self)
+
+    def on_message(self, message):
+        # 當有訊息進來時，將此訊息發送給其他 WebSocket
+        print ' [x] send message.'
+        for user in ChatManager.users:
+            user.write_message('You send: '+message)
+
+# 相關設定參數
+settings = dict(
+    debug=True,
+    autoreload=True,
+    compiled_template_cache=False,
+    static_path=os.path.join(os.path.dirname(__file__), "static"),
+    template_path=os.path.join(os.path.dirname(__file__), "templates")
+)
+
+class Application(web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", Chat),
+            (r"/socket", Socket)
+        ]
+        web.Application.__init__(self, handlers, **settings)
+
+def main():
+    options.parse_command_line()
+    app = Application()
+    app.listen(options.port, options.ip)
+    ioloop.IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
