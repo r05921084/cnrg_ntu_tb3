865b5f4f7684e04b620462db7ba522fcbc3cf104
YYliu <mikecool77882000@gmail.com>
2018-05-08 13:58:13 +0800

    add text_analysis package
    separate text_analysis node from chatbot package
    
    Former-commit-id: 6263dd09d00e2e4e40db0f979bc1be64810d4b49 [formerly 6b608c43c42783a1b607cebcc8ee97872126aad6]
    Former-commit-id: 296c636fd04ddadd2314c7a86290c67aa9e68c5f
    Former-commit-id: f44adf5a5d84d15c54203f9e7cc578edb1c7a4c6

---
+++ text_analysis/src/text_analysis.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from rospy import init_node, is_shutdown
+import rospy
+from std_msgs.msg import String
+from chatbot.msg import ChatterStamped
+import syntactic_extract
+import gensim
+import numpy as np
+import rospkg
+
+sentence_list = [] # save the talker's talks
+
+rospack = rospkg.RosPack()
+path = rospack.get_path('text_analysis')
+
+w2v_model = syntactic_extract.load_wordvec_model(path+'/wordvec_model/100features_20context_20mincount_zht')
+print('load model success')
+
+def call_back(data):
+	rospy.loginfo(rospy.get_caller_id() + 'I heard:\n%s', data.text)
+	sentence_list.append(data.text)
+	# print(str(sentence_list).decode('string_escape'))
+	if data.text=='analysis':
+		sentence_list.pop()
+		for s in sentence_list:
+			if s[0].isalpha():
+				pass
+			else:
+				print(syntactic_extract.pos_tag_analysis(s))
+				print(syntactic_extract.semantic_analysis(s, w2v_model))
+
+def text_analysis():
+	rospy.init_node('text_analysis', anonymous=True)
+	rospy.Subscriber('chatbot/input', ChatterStamped, call_back)
+	rospy.spin()
+
+if __name__ == '__main__':
+	text_analysis()
