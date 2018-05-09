af297e7ca43ea67e9415ac45950a1d2d69f63494
Jerry Chang <r05942055@ntu.edu.tw>
2018-05-09 16:03:33 +0800

    modified:   acoustic_anomaly/scripts/acoustic_anomaly_detector.py
    Former-commit-id: 63ecfd61dfa5eed86765f04dee7b3dc768979fea [formerly ad1cc32b192aab39e13973c65c9d17558fb34a51]
    Former-commit-id: 27674e5060d9cc09c44d7935544c34453eda2632
    Former-commit-id: 484756db75cb07e6f0ae459d7937cd54a67e76bf

--- acoustic_anomaly/scripts/acoustic_anomaly_detector.py
+++ acoustic_anomaly/scripts/acoustic_anomaly_detector.py
@@ -13,11 +13,37 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 from std_msgs.msg import String
 from std_msgs.msg import Float64MultiArray
+from chatbot.msg import ChatterStamped
 from binaural_microphone.msg import BinauralAudio
 
 
 rospack = rospkg.RosPack()
 path = rospack.get_path('acoustic_anomaly')
+NODE_NAME = 'acoustic_anomaly_detector'
+frames = []
+fs = 22050
+event = threading.Event()
+
+### load data from files
+param_path = os.path.join(path, 'param')
+embedded_model = load_model(os.path.join(param_path, 'embedded_model.h5'))
+embedded_model._make_predict_function()
+mean = np.load(os.path.join(param_path, 'NPYs/mean.npy'))
+std = np.load(os.path.join(param_path, 'NPYs/std.npy'))
+normal_SP = np.load(os.path.join(param_path, 'NPYs/Normal.npy')).reshape(1, -1)
+abnormal_SP = np.load(os.path.join(param_path, 'NPYs/Abnormal.npy')).reshape(1, -1)
+
+
+def pub_to_chatbot(text, priority=0):
+    msg = ChatterStamped(
+        header=Header(
+            stamp=rospy.Time.now()
+            ),
+        priority=priority,
+        source=NODE_NAME,
+        text=text,
+    )
+    publisher.publish(msg)
 
 
 def storing_from_array(data):
@@ -30,7 +56,6 @@ def storing_from_array(data):
 
 def model():
     global frames
-
     ### take the first 0.1(sec)*fs(Hz) points to make prediction
     if len(frames) < 2205:
         return None
@@ -55,9 +80,9 @@ def model():
         rospy.loginfo('end model')
 
         if sim_abnormal > sim_normal: 
-            return 'abnormal'
+            return 'ABNORMAL !!!'
         else: 
-            return 'normal'
+            return 'NORMAL'
 
 
 def get_raw_audio():
@@ -66,17 +91,24 @@ def get_raw_audio():
 
 
 def talker():
-    array = Float64MultiArray()
+    global publisher
+    publisher = rospy.Publisher('chatbot/output', ChatterStamped, queue_size=1)
+    pub_to_chatbot(text='start monitoring ...')
+
     pub = rospy.Publisher('Acoustic_Anomaly', Float64MultiArray, queue_size=10)
+    array = Float64MultiArray()
+
     def process():
         ab = model()
         if ab:
-            if ab == 'normal':
+            if ab == 'NORMAL':
                 array.data = [0]
             else:
                 array.data = [1]
             rospy.loginfo(ab)
             pub.publish(array)
+            pub_to_chatbot(text=ab)
+
     while not rospy.is_shutdown():
         while event.wait(1.0):
             event.clear()
@@ -84,23 +116,9 @@ def talker():
         print('timeout!')
 
 
-frames = []
-fs = 22050
-event = threading.Event()
-
-### load data from files
-param_path = os.path.join(path, 'param')
-embedded_model = load_model(os.path.join(param_path, 'embedded_model.h5'))
-embedded_model._make_predict_function()
-mean = np.load(os.path.join(param_path, 'NPYs/mean.npy'))
-std = np.load(os.path.join(param_path, 'NPYs/std.npy'))
-normal_SP = np.load(os.path.join(param_path, 'NPYs/Normal.npy')).reshape(1, -1)
-abnormal_SP = np.load(os.path.join(param_path, 'NPYs/Abnormal.npy')).reshape(1, -1)
-
-
 if __name__ == '__main__':
     try:
-        rospy.init_node('acoustic_anomaly_detector', anonymous=True)
+        rospy.init_node(NODE_NAME, anonymous=True)
         get_raw_audio()
         talker()
     except rospy.ROSInterruptException:
